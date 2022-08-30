ef4c66347600ee232ae11a2852bd838b6d0afb4c
cecilia1 <belindazhao0130@gmail.com>
2022-08-30 08:00:12 -0700

    lyft

---
+++ addyear.py
@@ -0,0 +1,3 @@
+def addyear(pre_date, years_to_add):
+    result=pre_date.replace(year=pre_date.year+years_to_add)
+    return result
---
+++ battery/SpindlerBattery.py
@@ -0,0 +1,13 @@
+from battery.battery import Battery
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        if addyear(self.last_service_date,3) < self.current_date:
+            return True
+        else:
+            return False
