90f84b43fa32b5a6fc96831c50a88bd03fb8286d
cecilia1 <belindazhao0130@gmail.com>
2022-08-30 08:44:44 -0700

    lyft

--- battery/SpindlerBattery.py
+++ battery/SpindlerBattery.py
@@ -7,7 +7,7 @@ class SpindlerBattery(Battery):
         self.last_service_date = last_service_date
 
     def needs_service(self):
-        if addyear(self.last_service_date,3) < self.current_date:
+        if addyear(self.last_service_date,2) < self.current_date:
             return True
         else:
             return False
